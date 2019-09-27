825b0e6e6e9bb79aff45a09a73c2340778ee1360
lsalazar1 <lmsalazar78011@gmail.com>
2019-09-27 16:00:05 -0500

    JSON Web Token implemented

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 // Bring in user model
@@ -62,8 +64,20 @@ router.post(
             await user.save();
 
             // Return jsonwebtoken
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
 
-            res.send('User registered');
+            jwt.sign(
+                payload,
+                config.get('jwtToken'),
+                { expiresIn: 360000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                });
         } catch (err) {
             console.error(err.message);
             res.status(500).send('Server Error');
