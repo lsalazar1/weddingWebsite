bc6f6ab1257551330fe798c1b87225e0a9055eb0
lsalazar1 <lmsalazar78011@gmail.com>
2019-11-28 04:11:34 -0600

    Edited code for clear-profile edge case

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -8,7 +8,8 @@ import {
     AUTH_ERROR,
     LOGIN_FAIL,
     LOGIN_SUCCESS,
-    LOGOUT
+    LOGOUT,
+    CLEAR_PROFILE
 } from './types';
 
 // Load User
@@ -97,5 +98,6 @@ export const login = (email, password) => async dispatch => {
 
 // Logout and clear profile
 export const logout = () => dispatch => {
+    dispatch({ type: CLEAR_PROFILE });
     dispatch({ type: LOGOUT });
 }
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,15 +1,35 @@
 import React, { Fragment, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Spinner from '../layout/Spinner';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profile';
 
-const Dashboard = ({ getCurrentProfile, auth, profile: { profile, loading } }) => {
+const Dashboard = ({ 
+    getCurrentProfile, 
+    auth: { user }, 
+    profile: { profile, loading } 
+}) => {
     useEffect(() => {
         getCurrentProfile();
     }, []);
 
-    return loading && profile === null ? <Spinner /> : <Fragment>test</Fragment>
+    return loading && profile === null ? <Spinner /> : <Fragment>
+        <h1 className="large text-primary">Dashboard</h1>
+        <p className="lead">
+            <i className="fas fa-user"></i> Welcome { user && user.name}
+        </p>
+        {profile !== null ? (
+            <Fragment>has</Fragment>
+        ) : (
+            <Fragment>
+                <p>You haven't setup your profile, please do so</p>
+                <Link to="/create-profile" className="btn btn-primary my-1">
+                    Create Profile
+                </Link>
+            </Fragment>
+        )}
+    </Fragment>
 };
 
 Dashboard.propTypes = {
