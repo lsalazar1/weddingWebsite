14be557a47c2f67328a469d7f773ea2fea412190
lsalazar1 <lmsalazar78011@gmail.com>
2019-11-29 03:33:26 -0600

    Creating functionality to add experiences and education for profiles

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from '../actions/types';
+import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE } from '../actions/types';
 
 const initialState = {
     profile: null,
@@ -13,18 +13,19 @@ export default function(state = initialState, action) {
 
     switch (type) {
         case GET_PROFILE:
+        case UPDATE_PROFILE:
             return {
                 ...state,
                 profile: payload,
                 loading: false
-            }
-        
+            };
+  
         case PROFILE_ERROR:
             return {
                ...state,
                error: payload,
                loading: false     
-            }
+            };
 
         case CLEAR_PROFILE:
             return {
@@ -32,7 +33,7 @@ export default function(state = initialState, action) {
                 profile: null,
                 repos: [],
                 loading: false
-            }
+            };
 
         default:
             return state;
