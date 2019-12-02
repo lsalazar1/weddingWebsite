fd5742cfbab9fc0ada87c84d896e5a6de5550f7d
lsalazar1 <lmsalazar78011@gmail.com>
2019-12-02 00:14:51 -0600

    Adding Reducer, Action, and initial component for posts

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -16,4 +16,6 @@ export const GET_PROFILES = 'GET_PROFILES';
 export const PROFILE_ERROR = 'PROFILE_ERROR';
 export const CLEAR_PROFILE = 'CLEAR_PROFILE';
 export const UPDATE_PROFILE = 'UPDATE_PROFILE';
-export const GET_REPOS = 'GET_REPOS';
+
+export const GET_POSTS = 'GET_POSTS';
+export const POST_ERROR = 'POST_ERROR';
