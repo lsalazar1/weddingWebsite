5519a1081e89435b5e067aa12327c2303bc373d9
lsalazar1 <lmsalazar78011@gmail.com>
2019-12-07 18:36:25 -0600

    Adding action for posting comments

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -6,7 +6,9 @@ import {
     UPDATE_LIKES, 
     DELETE_POST,
     ADD_POST,
-    GET_POST 
+    GET_POST,
+    ADD_COMMENT,
+    REMOVE_COMMENT 
 } from './types';
 
 // Get posts
@@ -120,3 +122,48 @@ export const getPost = id => async dispatch => {
         });
     }
 };
+
+// Add comment
+export const addComment = (postId, formData) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    try {
+        const res = await axios.post(`/api/posts/comment/${postId}`, formData, config);
+
+        dispatch({
+            type: ADD_COMMENT,
+            payload: res.data
+        });
+
+        dispatch(setAlert('Comment Posted!', 'success'));
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
+
+// Delete comment
+export const deleteComment = (postId, commentId) => async dispatch => {
+    try {
+        const res = await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+
+        dispatch({
+            type: REMOVE_COMMENT,
+            payload: commentId
+        });
+
+        dispatch(setAlert('Comment Removed!', 'success'));
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
+
