0cb10e91418bcebb60ef0c62468008003119e093
lsalazar1 <lmsalazar78011@gmail.com>
2019-12-31 16:43:43 -0500

    Adding Image Picker Component for Our-Story page

--- client/src/components/our-story/OurStory.js
+++ client/src/components/our-story/OurStory.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import engagement from './engagement.jpeg';
+import RandomPicture from './RandomPicture';
 
 const OurStory = () => {
     return (
@@ -8,12 +8,12 @@ const OurStory = () => {
             <p className="lead">
                 <i className="fas fa-book" />{' '} Meeting on the Internet & Overcoming Long Distance Relationships 
             </p>
-            <img alt="" src={engagement}/>
+            <RandomPicture />
             <div className="my-2 p-1 bg-white no-cursive">
                 <p className="my-1 tab margin-right">
-                    Believe it or not, we both met on a popular blogging website called Tumblr. We were both following a 
-                    penpal blog, where each blog post was a bio of a person. On March 22, 2014, we exchanged information and started
-                    texting and Skyping. This became a daily habit after two months. We decided to be more than just friends, despite the 2500Km that seperated us.
+                    Sandra and Liam met on a popular blogging website called Tumblr. They were both following a 
+                    penpal blog, where each blog post was a bio of a person. Liam became interested Sandra's post and requested the blog owner for her contact information. On 
+                    March 22, 2014, they exchanged information and started texting and Skyping. This became a daily habit after two months. Sandra and Liam decided to be more than just friends, despite the 2500Km that seperated them.
                 </p>
                 <p className="my-1 tab margin-right">
                     Liam would start taking one of many trips to and from Canada (two by GreyHound, yuck). We first met in person on 
