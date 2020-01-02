e7a3f8ac6055b3190f86bd3ea0db915e67bcac35
lsalazar1 <lmsalazar78011@gmail.com>
2020-01-02 03:21:02 -0500

    Removing portrait pics

--- client/src/components/our-story/RandomPicture.js
+++ client/src/components/our-story/RandomPicture.js
@@ -3,22 +3,15 @@ import React, { Fragment } from 'react';
 // Pictures used for slideshow
 import engagement1 from './assets/engagement1.jpeg';
 import engagement2 from './assets/engagement2.jpeg';
-import engagement3 from './assets/engagement3.jpeg';
 import engagement4 from './assets/engagement4.jpeg';
-import engagement5 from './assets/engagement5.jpeg';
 import engagement6 from './assets/engagement6.jpeg';
-import engagement7 from './assets/engagement7.jpeg';
-
 
 export const RandomPicture = () => {
     const imageArray = [
         engagement2, 
         engagement1, 
-        engagement3, 
         engagement4, 
-        engagement5, 
         engagement6, 
-        engagement7
     ];
 
     let imagePicker = imageArray[Math.floor(Math.random()*imageArray.length)];
